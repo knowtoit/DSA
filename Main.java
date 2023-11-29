d78f0ab256b67dee377b1d95189d240f27ef3e93
Aishwarya <saichaudharid@gmail.com>
2023-11-29 09:39:20 +0530

    first java code

---
+++ Main.java
@@ -0,0 +1,20 @@
+// import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Scanner = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+
+        // int sum = a + b;
+
+        System.out.println("sum");
+    }
+}
+
+// System.out.println("Hello World! Enjoy with java");
+// int a = 10;
+// int b = 5;
+// int ans = (a * b) / (a - b);
+// System.out.println(ans);
