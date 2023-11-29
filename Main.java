bc1f47a34ee874f761e1463525c723c64bf11529
Aishwarya <saichaudharid@gmail.com>
2023-11-29 12:25:50 +0530

    sc.close() use

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
-// import java.util.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Scanner = new Scanner(System.in);
-        // int a = sc.nextInt();
-        // int b = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        // int sum = a + b;
+        int sum = a + b;
 
-        System.out.println("sum");
+        System.out.println(sum);
+
+        sc.close();
     }
 }
 
