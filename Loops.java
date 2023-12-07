cdb127b0451743f20d12b1e83e14f37ee50dbde3
Aishwarya <saichaudharid@gmail.com>
2023-12-07 18:10:03 +0530

    While loop

--- Loops.java
+++ Loops.java
@@ -2,7 +2,13 @@
 public class Loops {
 
     public static void main(String[] args) {
-        for (int count = 0; count < 3; count++)
-            System.out.println("Hello world!");
+        // for (int count = 0; count < 3; count++)
+        // System.out.println("Hello world!");
+
+        int i = 0;
+        while (i < 11) {
+            System.out.print(i + " ");
+            i++;
+        }
     }
 }
