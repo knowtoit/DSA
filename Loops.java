179fd62699f97caa1900615514f9e742822bdabb
Aishwarya <saichaudharid@gmail.com>
2023-12-07 18:25:17 +0530

    Sum of n natural numbers

--- Loops.java
+++ Loops.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class Loops {
 
@@ -11,10 +12,20 @@ public class Loops {
         // i++;
         // }
 
-        int i = 0;
-        do {
-            System.out.print(i + " ");
-            i++;
-        } while (i < 11);
+        // int i = 0;
+        // do {
+        // System.out.print(i + " ");
+        // i++;
+        // } while (i < 11);
+
+        // Sum of n natural numbers
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum = sum + i;
+        }
+        System.out.print(sum + " ");
+        sc.close();
     }
 }
